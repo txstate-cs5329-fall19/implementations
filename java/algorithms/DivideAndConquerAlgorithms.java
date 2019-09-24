b534406c4d9c66c4195318294ad5a46bce2cd9c9
boris <borislav.sabotinov@gmail.com>
2019-09-23 22:25:09 -0500

    Adding implementation for Find Maximum Subarray divide and conquer algorithm.

---
+++ java/algorithms/DivideAndConquerAlgorithms.java
@@ -0,0 +1,6 @@
+package algorithms;
+
+public interface DivideAndConquerAlgorithms {
+    int findMaxCrossingSubarray(int[] array, int low, int mid, int high);
+    int findMaxSubarray(int[] array, int low, int high);
+}
